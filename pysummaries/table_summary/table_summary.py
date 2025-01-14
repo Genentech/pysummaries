a573f510f9aa81bd577a029e2ff4df53d96f7295
Otto Fajardo <otto.fajardob@gmail.com>
2025-01-14 17:01:12 +0000

    fixing adding Missing category only when needed

--- pysummaries/table_summary/table_summary.py
+++ pysummaries/table_summary/table_summary.py
@@ -65,7 +65,7 @@ def calculate_stats(df, var, functions, coltype, strata=None, stratcat=None, var
         curseries = df.loc[df[strata]==stratcat, var]
     if coltype=='categorical' and  categorical_missing_level:
         curseries = curseries.copy()
-        if curseries.dtype.name=='category':
+        if curseries.dtype.name=='category' and categorical_missing_level not in curseries.cat.categories.to_list():
             cats = curseries.cat.categories.to_list() + [categorical_missing_level]
             curseries = curseries.cat.set_categories(cats)
         curseries = curseries.fillna(categorical_missing_level)
